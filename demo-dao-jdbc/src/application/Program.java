b81c417d93b2dcbc6ae8c7e529c1708b76f4a738
Davi <davi.lima@ucsal.edu.br>
2021-03-11 20:43:48 -0300

    SellerDaoJDBC and DaoFactory

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -5,6 +5,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,15 +14,17 @@ public class Program {
 
 	public static void main(String[] args) throws ParseException {
 
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		Scanner sc = new Scanner(System.in);
+//		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+//		Scanner sc = new Scanner(System.in);
 
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
 
-		Date birthDate = sdf.parse(sc.nextLine());
-		Seller obj2 = new Seller(1, "João", "joão@", birthDate, 2000.00, obj);
+		Seller obj2 = new Seller(1, "João", "joão@", new Date(), 2000.00, obj);
 		System.out.println(obj2);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+//		sc.close();
 	}
 
 }
