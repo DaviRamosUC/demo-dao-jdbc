021c8d55be466ff2ae4fb185614561f3d7302800
Davi <davi.lima@ucsal.edu.br>
2021-03-11 20:29:15 -0300

    Seller entity class

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -1,14 +1,26 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws ParseException {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Scanner sc = new Scanner(System.in);
+
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
 
+		Date birthDate = sdf.parse(sc.nextLine());
+		Seller obj2 = new Seller(1, "João", "joão@", birthDate, 2000.00, obj);
+		System.out.println(obj2);
 	}
 
 }
