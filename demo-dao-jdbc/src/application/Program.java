707a01679d785220ba484d08810ff90436df2fab
Davi <davi.lima@ucsal.edu.br>
2021-03-11 22:18:27 -0300

    Update Implementation

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -35,10 +35,17 @@ public class Program {
 		}
 
 		System.out.println();
-		System.out.println("===== TEST 4: seller findAll =====");
+		System.out.println("===== TEST 4: seller insert =====");
 		Seller newSeller = new Seller(null, "Greg", "greg@", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = "+ newSeller.getId());
+
+		System.out.println();
+		System.out.println("===== TEST 5: seller update =====");
+		seller = sellerDao.findById(2);
+		seller.setName("Marta Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
 		
 	}
 
